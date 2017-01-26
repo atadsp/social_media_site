53551d67c76cb200e8fa6b17a2878128679f1c5c
Chris Beckman <atadsp@gmail.com>
2017-01-26 14:04:52 -0500

    testing commits on new pc

---
+++ server/routes/post.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../database/database');
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+router.use(function(req, res, next){
+	var token = req.headers['auth-token'];
+	jwt.verify(token, process.env.SECRET, function(err, decoded){
+		if(err){
+			res.status(400).send('Invalid Token');
+		} else {
+			req.user_id = decoded.id;
+			next();
+		}
+	});
+});
